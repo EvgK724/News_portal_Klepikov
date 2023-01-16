301c43f54794f55cdb4b6424fd3054dd0a8bba67
EvgK724 <ekle724@mail.ru>
2023-01-17 00:46:20 +0500

    Init News_portal_2

--- newspaper/portal/models.py
+++ newspaper/portal/models.py
@@ -53,6 +53,9 @@ class Post(models.Model):
     def preview(self):
         return self.text[0:124] + '...'
 
+    def __str__(self):
+        return f'{self.title}|{self.text[:20]}'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -73,6 +76,11 @@ class Comment(models.Model):
     def dislike(self):
         self.rating -= 1
         self.save()
+
+    def __str__(self):
+        return f'id={self.pk} name={self.text}'
+
+
 
 
 
